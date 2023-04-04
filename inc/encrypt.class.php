3ff5a00f8c87358467b2227f891d3c893498bb4b
Charlene Auger <charlene.auger@factorfx.com>
2023-04-04 09:51:51 +0000

    Update comment

--- inc/encrypt.class.php
+++ inc/encrypt.class.php
@@ -1,13 +1,25 @@
 <?php
-/*** 
--------------------------------------------------------------------------
- encryptfile plugin for GLPI
--------------------------------------------------------------------------
- ***/
+/**
+ * @package     encryptfile
+ * @author      Charlene Auger
+ * @copyright   Copyright (c) 2015-2023 FactorFX
+ * @license     AGPL License 3.0 or (at your option) any later version
+ *              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link        https://www.factorfx.com
+ * @since       2023
+ *
+ * --------------------------------------------------------------------------
+ */
 
 class PluginEncryptfileEncrypt extends CommonDBTM {
     static $rightname = 'plugin_encryptfile_encrypt';
-
+    
+    /**
+     * getTypeName
+     *
+     * @param  mixed $nb
+     * @return void
+     */
     public static function getTypeName($nb = 1) {
         return _n('Encrypted file', 'Encrypted files', $nb, 'encryptfile');
     }
