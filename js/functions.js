5a7075b32410926278e4ceebfc2ebdb5bef4db2b
Charlene Auger <charlene.auger@factorfx.com>
2023-04-19 12:23:51 +0000

    Add js functions file

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+/**
+ * @package     encryptfile
+ * @author      Charlene Auger
+ * @copyright   Copyright (c) 2015-2023 FactorFX
+ * @license     AGPL License 3.0 or (at your option) any later version
+ *              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link        https://www.factorfx.com
+ * @since       2023
+ *
+ * --------------------------------------------------------------------------
+ */
+
+$(document).ready(function() {
+   /* $('a').each(function() {
+        console.log($(this).prop('href'));
+        if ($(this).is('[href*="docid"')) {
+            alert("Contains questionmark");
+        }
+    });*/
+});
+
