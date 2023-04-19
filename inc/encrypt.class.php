ba8b1564990dfac6a4ee941a41cd6fc80dca5154
Charlene Auger <charlene.auger@factorfx.com>
2023-04-19 12:23:38 +0000

    Add encryptfile and decryptfile function

--- inc/encrypt.class.php
+++ inc/encrypt.class.php
@@ -30,7 +30,7 @@ class PluginEncryptfileEncrypt extends CommonDBTM {
      * @return void
      */
     public function generateKey() {
-        return base64_encode(sodium_crypto_secretbox_keygen());
+        return base64_encode(sodium_crypto_secretstream_xchacha20poly1305_keygen());
     }
     
     /**
@@ -39,7 +39,147 @@ class PluginEncryptfileEncrypt extends CommonDBTM {
      * @param  mixed $key
      * @return void
      */
-    public function decryptkey($key) {
-        return base64_decode($key);
+    static function decryptkey($secretKey) {
+        return base64_decode($secretKey);
+    }
+    
+    /**
+     * encryptFile
+     *
+     * @param  mixed $secretKey
+     * @param  mixed $filename
+     * @return void
+     */
+    static private function encryptFile($secretKey, $filename) {
+        $inputFile = GLPI_TMP_DIR.'/'.$filename;
+        $encryptedFile = GLPI_TMP_DIR.'/'.$filename.'.enc';
+        $chunkSize = 4096;
+
+        $fdIn = fopen($inputFile, 'rb');
+        $fdOut = fopen($encryptedFile, 'wb');
+
+        [$stream, $header] = sodium_crypto_secretstream_xchacha20poly1305_init_push($secretKey);
+
+        fwrite($fdOut, $header);
+
+        $tag = SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_TAG_MESSAGE;
+        do {
+            $chunk = fread($fdIn, $chunkSize);
+
+            if (feof($fdIn)) {
+                $tag = SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_TAG_FINAL;
+            }
+
+            $encryptedChunk = sodium_crypto_secretstream_xchacha20poly1305_push($stream, $chunk, '', $tag);
+            fwrite($fdOut, $encryptedChunk);
+        } while ($tag !== SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_TAG_FINAL);
+
+        fclose($fdOut);
+        fclose($fdIn);
+
+        if($chunk) {
+            rename($encryptedFile, $inputFile);
+            return true;
+        }
+    }
+    
+    /**
+     * decryptFile
+     *
+     * @param  mixed $secretKey
+     * @param  mixed $filepath
+     * @return void
+     */
+    static private function decryptFile($secretKey, $filepath) {
+        $encryptedFile = $filepath;
+        $decryptedFile = GLPI_TMP_DIR.'/example.dec';
+        $chunkSize = 4096;
+
+        $fdIn = fopen($encryptedFile, 'rb');
+        $fdOut = fopen($decryptedFile, 'wb');
+
+        $header = fread($fdIn, SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_HEADERBYTES);
+
+        $stream = sodium_crypto_secretstream_xchacha20poly1305_init_pull($header, $secretKey);
+
+        do {
+            $chunk = fread($fdIn, $chunkSize + SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_ABYTES);
+            [$decryptedChunk, $tag] = sodium_crypto_secretstream_xchacha20poly1305_pull($stream, $chunk);
+
+            fwrite($fdOut, $decryptedChunk);
+        } while ((!feof($fdIn) && $tag !== SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_TAG_FINAL) || !$chunk);
+
+        $ok = feof($fdIn);
+
+        fclose($fdOut);
+        fclose($fdIn);
+
+        if (!$ok) {
+            die('Invalid/corrupted input');
+        } else {
+            rename($decryptedFile, $encryptedFile);
+            return true;
+        }
+    }
+    
+    /**
+     * beforeAddDocument
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    static function beforeAddDocument(Document $post) {
+        if(Session::haveRight("plugin_encryptfile_encrypt", UPDATE)) {
+            $PluginEncryptfileConfig = new PluginEncryptfileConfig();
+            $secretKey = $PluginEncryptfileConfig->getSecretKey($_SESSION["glpiactiveprofile"]["id"]);
+
+            if(!is_null($secretKey)) {
+                PluginEncryptfileEncrypt::encryptFile(PluginEncryptfileEncrypt::decryptkey($secretKey), $post->input["_filename"][0]);
+            }
+        }
+    }
+    
+    /**
+     * afterAddDocument
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    static function afterAddDocument(Document $post) {
+        if(Session::haveRight("plugin_encryptfile_encrypt", UPDATE)) {
+            $PluginEncryptfileConfig = new PluginEncryptfileConfig();
+            $secretKeyId = $PluginEncryptfileConfig->getSecretKeyId($_SESSION["glpiactiveprofile"]["id"]);
+
+            if(!is_null($secretKeyId)) {
+                $PluginEncryptfileConfig->saveDocumentInfo($secretKeyId, $post->fields["id"]);
+            }
+        }
+    }
+    
+    /**
+     * beforeDownloadDocument
+     *
+     * @param  mixed $documentId
+     * @param  mixed $filepath
+     * @return void
+     */
+    static function beforeDownloadDocument($documentId, $filepath) {
+        if(Session::haveRight("plugin_encryptfile_encrypt", READ)) {
+            $PluginEncryptfileConfig = new PluginEncryptfileConfig();
+
+            // Check if it is an encrypted file
+            $secretKeyId = $PluginEncryptfileConfig->isEncrypted($documentId);
+            
+            if($secretKeyId) {
+                $secretKey = $PluginEncryptfileConfig->getSecretKey($_SESSION["glpiactiveprofile"]["id"], $secretKeyId);
+
+                // If secretKey empty, maybe it have only read right
+                if(is_null($secretKey)) $secretKey = $PluginEncryptfileConfig->canRead($_SESSION["glpiactiveprofile"]["id"], $secretKeyId);
+    
+                if(!is_null($secretKey)) {
+                    PluginEncryptfileEncrypt::decryptFile(PluginEncryptfileEncrypt::decryptkey($secretKey), $filepath);
+                }
+            }
+        }
     }
 } 
