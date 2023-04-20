ac197a807eea6b01e442fda2f618d44b33d02b6a
Charlene Auger <charlene.auger@factorfx.com>
2023-04-20 08:07:51 +0000

    Separate js functions in to files

---
+++ js/read.js
@@ -0,0 +1,23 @@
+/**
+ * @package     encryptfile
+ * @author      Charlene Auger
+ * @copyright   Copyright (c) 2015-2023 FactorFX
+ * @license     AGPL License 3.0 or (at your option) any later version
+ *              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link        https://www.factorfx.com
+ * @since       2023
+ *
+ * --------------------------------------------------------------------------
+ */
+
+$(document).ajaxStop(function() {
+    setTimeout(function() {
+        $('a').each(function() {
+            if ($(this).is('[href*="docid"')) {
+                var href = $(this).attr('href');
+                href = href.replace('front/document.send.php', 'plugins/encryptfile/front/document.send.php');
+                $(this).attr('href', href);
+            }
+        });
+    }, 500);
+});
---
+++ js/write.js
@@ -0,0 +1,42 @@
+/**
+ * @package     encryptfile
+ * @author      Charlene Auger
+ * @copyright   Copyright (c) 2015-2023 FactorFX
+ * @license     AGPL License 3.0 or (at your option) any later version
+ *              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link        https://www.factorfx.com
+ * @since       2023
+ *
+ * --------------------------------------------------------------------------
+ */
+
+$(document).ajaxStop(function() {
+    setTimeout(function() {
+        $('form').each(function() {
+            if ($(this).is('[action*="front/document.form.php"') && $('#encryptfile').length == 0) {
+                addEncryptedFileCheckbox($(this));
+            }
+        });
+    }, 500);
+});
+
+function addEncryptedFileCheckbox(form) {
+    var RegexUrl = /^(.*)front\/.*\.form\.php/;
+    var RegexUrlRes = RegexUrl.exec(window.location.pathname);
+
+    $.ajax({
+        url : RegexUrlRes[1] + 'plugins/encryptfile/ajax/getItem.php',
+        type : 'GET',
+        dataType : 'html',
+        data : {
+            'item_url' : RegexUrlRes[0]
+        },
+        success : function(data){
+            var input = form.find('input[name="add"]');
+            $(data).insertBefore(input[0]);
+        },
+        error : function () {
+            console.log("Error when attempt to insert encrypted file checkbox");
+        }
+    });
+}
